d9774c1689559f9316b0ff59094b331490ac8590
IceyWu <kui.wu@boran-tech.com>
2023-05-26 23:18:03 +0800

    fix: 移动端适配

--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import "./styles/main.css";
 // import router from './router'
 import { router } from "./router";
 import store from "./store";
+import "amfe-flexible";
 
 createApp(App).use(store).use(router).mount("#app");
