e4367f380c2992b0acaa422ec780d6384dec6490
IceyWu <3128006406@qq.com>
2023-12-07 10:07:21 +0800

    chore: update version

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ],
   plugins: [
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-nullish-coalescing-operator",
-    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-transform-nullish-coalescing-operator",
+    "@babel/plugin-transform-object-rest-spread",
+    "@babel/plugin-transform-optional-chaining",
   ]
 }
